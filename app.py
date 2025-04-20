9f5011ff928a25ec81dd81244fc1ffaa1274cd33
darkknightraj <rajankumarm30@gmail.com>
2025-04-20 20:30:14 +0530

    Day 2 april 20 modified

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World! hi i am rajan  "
+    return "Hello World! hi i am rajan  i want to learn more  "
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)  # ← Must use port 80!
